87f7d8a6d0c78b395864062ae2e5e0a6cb18b081
Endeavorer <rlgus0375@naver.com>
2022-08-02 09:22:41 +0900

    작업 진행 중

---
+++ src/com/dogather/dto/user/UserAddressDTO.java
@@ -0,0 +1,60 @@
+package com.dogather.dto.user;
+
+public class UserAddressDTO {
+	private int user_index;
+	private String zip_code;
+	private String address1;
+	private String address2;
+	private String address3;
+	private String address4;
+	private String address_detail;
+	private String address1_extra;
+	public int getUser_index() {
+		return user_index;
+	}
+	public void setUser_index(int user_index) {
+		this.user_index = user_index;
+	}
+	public String getZip_code() {
+		return zip_code;
+	}
+	public void setZip_code(String zip_code) {
+		this.zip_code = zip_code;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public String getAddress3() {
+		return address3;
+	}
+	public void setAddress3(String address3) {
+		this.address3 = address3;
+	}
+	public String getAddress4() {
+		return address4;
+	}
+	public void setAddress4(String address4) {
+		this.address4 = address4;
+	}
+	public String getAddress_detail() {
+		return address_detail;
+	}
+	public void setAddress_detail(String address_detail) {
+		this.address_detail = address_detail;
+	}
+	public String getAddress1_extra() {
+		return address1_extra;
+	}
+	public void setAddress1_extra(String address1_extra) {
+		this.address1_extra = address1_extra;
+	}
+}
